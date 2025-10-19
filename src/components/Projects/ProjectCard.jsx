ba25ea932f1c84df0a39417fdcca1a6f320a380b
Jared Tichacek <jared.tichacek@gmail.com>
2025-10-19 17:41:45 -0500

    added event-driven integration project, optimized projectcard with defensive checks

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -11,35 +11,45 @@ export default function ProjectCard({
         <div className="flex flex-col items-center reveal">
             <div className="mt-20 relative">
                 <div className="card w-100 h-100 image-full relative group mx-4 flex items-center justify-center">
-                    <figure>
-                        <img
-                            src={demo}
-                            alt={name}
-                            className="z-10 transition-opacity duration-300 object-cover group-hover:opacity-50"
-                        />
-                    </figure>
-                    <div className="card-body text-center inset-0 opacity-0 transition-opacity duration-300 z-20 group-hover:opacity-100">
-                        <p className="text-stone-200">{description}</p>
+                    {demo && (
+                        <figure>
+                            <img
+                                src={demo}
+                                alt={name}
+                                className="z-10 transition-opacity duration-300 object-cover group-hover:opacity-50"
+                            />
+                        </figure>
+                    )}
+                    {description && (
+                        <div className="card-body text-center inset-0 opacity-0 transition-opacity duration-300 z-20 group-hover:opacity-100">
+                            <p className="text-stone-200">{description}</p>
+                        </div>
+                    )}
+                </div>
+                {(deployment || github) && (
+                    <div className="flex justify-center mt-5 space-x-5">
+                        {deployment && (
+                            <a
+                                href={deployment}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="btn bg-code-green text-black hover:text-white"
+                            >
+                                View App
+                            </a>
+                        )}
+                        {github && (
+                            <a
+                                href={github}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="btn bg-code-green text-black hover:text-white"
+                            >
+                                View GitHub
+                            </a>
+                        )}
                     </div>
-                </div>
-                <div className="flex justify-center mt-5 space-x-5">
-                    <a
-                        href={deployment}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="btn bg-code-green text-black hover:text-white"
-                    >
-                        View App
-                    </a>
-                    <a
-                        href={github}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="btn bg-code-green text-black hover:text-white"
-                    >
-                        View GitHub
-                    </a>
-                </div>
+                )}
             </div>
         </div>
     );
--- src/projects.js
+++ src/projects.js
@@ -1,44 +1,50 @@
 export const projects = [
-    {
-      name: 'HydroHomies',
-      description: 'A MERN stack e-commerce site.',
-      demo: '/images/hydro-homies.gif',
-      deployment: 'https://hydrohomies.herokuapp.com/',
-      github: 'https://github.com/jtich40/hydro-homies'
-    },
-    {
-      name: 'Star Wars Galaxy Search',
-      description: 'A React Star Wars character search engine.',
-      demo: '/images/star-wars.gif',
-      deployment: 'https://starwarsgalaxysearch.netlify.app/',
-      github: 'https://github.com/jtich40/corinth-code-challenge'
-    },
-    {
-      name: 'Deep Thoughts',
-      description: 'A social media app hosted on AWS.',
-      demo: '/images/deep-thoughts.gif',
-      deployment: 'http://54.165.182.68/',
-      github: 'https://github.com/jtich40/aws-thought'
-    },
-    {
-      name: 'Tich-Tac-Toe',
-      description: 'A React Tic-Tac-Toe game.',
-      demo: '/images/tich-tac-toe.gif',
-      deployment: 'https://tichtactoe.netlify.app/',
-      github: 'https://github.com/jtich40/tich-tac-toe'
-    },
-    {
-      name: 'MIPHY',
-      description: 'A front-end search engine that finds GIFs from your favorite movies.',
-      demo: '/images/miphy.gif',
-      deployment: 'https://jtich40.github.io/miphy/',
-      github: 'https://github.com/jtich40/miphy'
-    },
-    {
-      name: 'Shared Lunch',
-      description: 'A full-stack MVC blog site to coordinate food donations.',
-      demo: '/images/shared-lunch.gif',
-      deployment: 'https://shared-lunch.herokuapp.com/',
-      github: 'https://github.com/jtich40/hydro-homies'
-    },
-  ]
+  {
+    name: 'Event-Driven Integration Demo',
+    description: 'A serverless app that mocks integrating a CRM with an ERP through event-driven flow.',
+    demo: '/images/event-driven-integration-demo.gif',
+    github: 'https://github.com/jtich40/event-driven-integration-demo'
+  },  
+  {
+    name: 'HydroHomies',
+    description: 'A MERN stack e-commerce site.',
+    demo: '/images/hydro-homies.gif',
+    deployment: 'https://hydrohomies.herokuapp.com/',
+    github: 'https://github.com/jtich40/hydro-homies'
+  },
+  {
+    name: 'Star Wars Galaxy Search',
+    description: 'A React Star Wars character search engine.',
+    demo: '/images/star-wars.gif',
+    deployment: 'https://starwarsgalaxysearch.netlify.app/',
+    github: 'https://github.com/jtich40/corinth-code-challenge'
+  },
+  {
+    name: 'Deep Thoughts',
+    description: 'A social media app hosted on AWS.',
+    demo: '/images/deep-thoughts.gif',
+    deployment: 'http://54.165.182.68/',
+    github: 'https://github.com/jtich40/aws-thought'
+  },
+  {
+    name: 'Tich-Tac-Toe',
+    description: 'A React Tic-Tac-Toe game.',
+    demo: '/images/tich-tac-toe.gif',
+    deployment: 'https://tichtactoe.netlify.app/',
+    github: 'https://github.com/jtich40/tich-tac-toe'
+  },
+  {
+    name: 'MIPHY',
+    description: 'A front-end search engine that finds GIFs from your favorite movies.',
+    demo: '/images/miphy.gif',
+    deployment: 'https://jtich40.github.io/miphy/',
+    github: 'https://github.com/jtich40/miphy'
+  },
+  {
+    name: 'Shared Lunch',
+    description: 'A full-stack MVC blog site to coordinate food donations.',
+    demo: '/images/shared-lunch.gif',
+    deployment: 'https://shared-lunch.herokuapp.com/',
+    github: 'https://github.com/jtich40/hydro-homies'
+  },
+]
